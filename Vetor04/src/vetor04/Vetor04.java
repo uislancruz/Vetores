697c23dad5b0726ba318fd0e8141821fd3af6fb4
Uislan Cruz <uislansk8@hotmail.com>
2021-10-21 21:21:39 -0300

    Testando a busca

--- Vetor04/src/vetor04/Vetor04.java
+++ Vetor04/src/vetor04/Vetor04.java
@@ -5,6 +5,8 @@
  */
 package vetor04;
 
+import java.util.Arrays;
+
 /**
  *
  * @author uisla
@@ -16,6 +18,13 @@ public class Vetor04 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int vet[] = {3,7,6,1,9,4,2};
+        for (int v:vet){
+            System.out.print(v+ " ");
+        }
+        System.out.println("");
+        int p = Arrays.binarySearch(vet, 1);
+        System.out.println("Encontrei o valor na posição "+ p);
     }
     
 }
